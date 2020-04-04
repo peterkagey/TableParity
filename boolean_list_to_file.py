718897e10f5a79e070f583f4ed5a26267a5ca7b1
Alec Jones <jones.alecj@gmail.com>
2020-04-04 10:54:22 -0700

    encapsulate boolean lists to class

--- boolean_list_to_file.py
+++ boolean_list_to_file.py
@@ -1,66 +1,60 @@
-from random import randrange
+class BitmapWriter:
 
-# This expects a list of exactly eight bools, but it doesn't check.
-def to_byte(boolean_list):
-    s = 0
-    for b in boolean_list:
-        s <<= 1
-        if b: s ^= 1
-    return s
+    def __init__(self, list_of_boolean_lists):
+        self.list_of_boolean_lists = list_of_boolean_lists[::-1]
+    
+    # This expects a list of exactly eight bools, but it doesn't check.
+    def to_byte(self, boolean_list):
+        s = 0
+        for b in boolean_list:
+            s <<= 1
+            if b: s ^= 1
+        return s
 
-# This expects the list to be a multiple of 8.
-def to_byte_array(boolean_list):
-    return bytes([to_byte(boolean_list[8*i:8*i+8]) for i in range(len(boolean_list)//8)])
+    # This expects the list to be a multiple of 8.
+    def to_byte_array(self, boolean_list):
+        return bytes([self.to_byte(boolean_list[8*i:8*i+8]) for i in range(len(boolean_list)//8)])
 
-def little_endian(value, byte_count):
-    return bytes([(value >> 8*i) & 0xFF for i in range(byte_count)])
+    def little_endian(self, value, byte_count):
+        return bytes([(value >> 8*i) & 0xFF for i in range(byte_count)])
 
-def random_data(byte_count):
-    data = []
-    for _ in range(byte_count):
-        data.append(randrange(256))
-    return bytes(data)
+    # 14 bytes
+    def bmp_file_header(self, width, height):
+        header_field     = bytes([0x42, 0x4D])
+        file_size        = self.little_endian(14 + 40 + 8 + (width * height) // 8, 4)
+        reserved         = self.little_endian(0, 4)
+        starting_address = self.little_endian(14 + 40 + 8, 4)
+        return header_field + file_size + reserved + starting_address
 
-# 14 bytes
-def bmp_file_header(width, height):
-    header_field     = bytes([0x42, 0x4D])
-    file_size        = little_endian(14 + 40 + 8 + (width * height) // 8, 4)
-    reserved         = little_endian(0, 4)
-    starting_address = little_endian(14 + 40 + 8, 4)
-    return header_field + file_size + reserved + starting_address
+    # BITMAPINFOHEADER
+    def dib_header(self, width, height):
+        size_of_header        = self.little_endian(40, 4) # 40 bytes
+        pixel_width           = self.little_endian(width, 4)
+        pixel_height          = self.little_endian(height, 4)
+        color_planes          = self.little_endian(1, 2)
+        bits_per_pixel        = self.little_endian(1, 2)
+        compression_method    = self.little_endian(0, 4) # BI_RGB (uncompressed)
+        image_size            = self.little_endian(0, 4) # a dummy 0 can be given for BI_RGB bitmaps.
+        horizontal_resolution = self.little_endian(255, 4)
+        vertical_resolution   = self.little_endian(255, 4)
+        number_of_colors      = self.little_endian(2, 4) # 0 to default to 2n
+        important_colors      = self.little_endian(0, 4) # 0 when every color is important
+        return size_of_header + pixel_width + pixel_height + color_planes + bits_per_pixel + compression_method + image_size + horizontal_resolution + vertical_resolution + number_of_colors + important_colors
 
-# BITMAPINFOHEADER
-def dib_header(width, height):
-    size_of_header        = little_endian(40, 4) # 40 bytes
-    pixel_width           = little_endian(width, 4)
-    pixel_height          = little_endian(height, 4)
-    color_planes          = little_endian(1, 2)
-    bits_per_pixel        = little_endian(1, 2)
-    compression_method    = little_endian(0, 4) # BI_RGB (uncompressed)
-    image_size            = little_endian(0, 4) # a dummy 0 can be given for BI_RGB bitmaps.
-    horizontal_resolution = little_endian(255, 4)
-    vertical_resolution   = little_endian(255, 4)
-    number_of_colors      = little_endian(2, 4) # 0 to default to 2n
-    important_colors      = little_endian(0, 4) # 0 when every color is important
-    return size_of_header + pixel_width + pixel_height + color_planes + bits_per_pixel + compression_method + image_size + horizontal_resolution + vertical_resolution + number_of_colors + important_colors
+    def color_table(self):
+        white = self.little_endian(0xffffffff, 4)
+        black = self.little_endian(0x00000000, 4)
+        return black + white
 
-def color_table():
-    white = little_endian(0xffffffff, 4)
-    black = little_endian(0x00000000, 4)
-    return white + black
+    def flatten(self, l):
+        return [item for sublist in l for item in sublist]
 
-def random_bmp(width, height):
-    return bmp_file_header(width, height) + dib_header(width, height) + color_table() + random_data((width * height)//8)
+    def bmp(self):
+        height = len(self.list_of_boolean_lists)
+        width  = len(self.list_of_boolean_lists[0])
+        bitMap = self.to_byte_array(self.flatten(self.list_of_boolean_lists))
+        return self.bmp_file_header(width, height) + self.dib_header(width, height) + self.color_table() + bitMap
 
-def flatten(l):
-    return [item for sublist in l for item in sublist]
-
-
-def bmp(list_of_boolean_lists):
-    height = len(list_of_boolean_lists)
-    width  = len(list_of_boolean_lists[0])
-    bitMap = to_byte_array(flatten(list_of_boolean_lists))
-    return bmp_file_header(width, height) + dib_header(width, height) + color_table() + bitMap
-
-# with open("example.bmp", 'wb') as output:
-    # output.write(random_bmp(2**9, 2**10))
+    def write_bitmap(self, file_name):
+        with open(file_name, 'wb') as output:
+            output.write(self.bmp())
