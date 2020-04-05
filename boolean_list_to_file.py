0d57415d34e3daa11ff1d73cf2ce6995df738c79
Peter Kagey <peterkagey@gmail.com>
2020-04-05 13:01:57 -0700

    Add support for image widths that are not multiples of 32 and vertically flip images.
    	# Remove .DS_Store
    	# Remove some .bmp images.

--- boolean_list_to_file.py
+++ boolean_list_to_file.py
@@ -1,3 +1,5 @@
+from functools import reduce
+
 class BitmapWriter:
 
     def __init__(self, list_of_boolean_lists):
@@ -11,9 +13,10 @@ class BitmapWriter:
             if b: s ^= 1
         return s
 
-    # This expects the list to be a multiple of 8.
-    def to_byte_array(self, boolean_list):
-        return bytes([self.to_byte(boolean_list[8*i:8*i+8]) for i in range(len(boolean_list)//8)])
+    def to_padded_byte_array(self, boolean_list):
+        pad_length = -len(boolean_list) % 32
+        padded_array = boolean_list + [False] * pad_length
+        return bytes([self.to_byte(padded_array[8*i:8*i+8]) for i in range(len(padded_array)//8)])
 
     def little_endian(self, value, byte_count):
         return bytes([(value >> 8*i) & 0xFF for i in range(byte_count)])
@@ -21,7 +24,8 @@ class BitmapWriter:
     # 14 bytes
     def bmp_file_header(self, width, height):
         header_field     = bytes([0x42, 0x4D])
-        file_size        = self.little_endian(14 + 40 + 8 + (width * height) // 8, 4)
+        padded_width     = width + (-width % 32)
+        file_size        = self.little_endian(14 + 40 + 8 + (padded_width * height) // 8, 4)
         reserved         = self.little_endian(0, 4)
         starting_address = self.little_endian(14 + 40 + 8, 4)
         return header_field + file_size + reserved + starting_address
@@ -46,14 +50,14 @@ class BitmapWriter:
         black = self.little_endian(0x00000000, 4)
         return black + white
 
-    def flatten(self, l):
-        return [item for sublist in l for item in sublist]
+    def pixel_data(self):
+        list_of_byte_arrays = map(self.to_padded_byte_array, self.list_of_boolean_lists)
+        return reduce(lambda b1, b2: b2 + b1, list_of_byte_arrays)
 
     def bmp(self):
         height = len(self.list_of_boolean_lists)
         width  = len(self.list_of_boolean_lists[0])
-        bitMap = self.to_byte_array(self.flatten(self.list_of_boolean_lists))
-        return self.bmp_file_header(width, height) + self.dib_header(width, height) + self.color_table() + bitMap
+        return self.bmp_file_header(width, height) + self.dib_header(width, height) + self.color_table() + self.pixel_data()
 
     def write_bitmap(self, file_name):
         with open(file_name, 'wb') as output:
